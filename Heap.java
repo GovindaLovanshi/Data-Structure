948149f1b807c3ab71562958446d7b8312a6620e
Govinda Lovanshi <134533021+GovindaLovanshi@users.noreply.github.com>
2024-02-07 15:40:36 +0530

    New Question Sliding Window  and  Weakest Soldier

--- Heap.java
+++ Heap.java
@@ -177,6 +177,177 @@ public class Heap {
         return cnt;
     }
 
+    // Two Sum
+
+    // Heap sort
+    public static void Heapify(int arr[], int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxIndex = i;
+
+        if (left < size && arr[maxIndex] < arr[left]) {
+            maxIndex = left;
+        }
+        if (right < size && arr[right] > arr[maxIndex]) {
+            maxIndex = right;
+        }
+
+        if (maxIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+            Heapify(arr, maxIndex, size);
+        }
+    }
+
+    public static void HeapSort(int arr[]) {
+        int n = arr.length;
+        for (int i = n / 2; i >= 0; i--) {
+            Heapify(arr, i, n);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            Heapify(arr, 0, i);
+        }
+    }
+
+    // Sliding Window Maximumm maximum of all sub array
+
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
+
+        public Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            // ascending order
+            // return this.val - p2.val;
+            // decending order
+            return p2.val - this.val;
+        }
+    }
+
+    // weakest soldier
+    static class Row implements Comparable<Row> {
+        int soldier;
+        int index;
+
+        public Row(int soldier, int index) {
+            this.soldier = soldier;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Row r2) {
+            if (this.soldier == r2.soldier) {
+                return this.index - r2.index;
+            } else {
+                return this.soldier - r2.soldier;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // p = new PriorityQueue<>();
+        // k = 3;
+        // int arr[] = { 10, 20, 11, 70, 50, 40, 100, 5, };
+        // List<Integer> list = KthLargest(arr);
+        // for (int x : list) {
+        // System.out.print(x + " ");
+        // }
+
+        // int N = 6;
+        // int arr[] = { 2, 6 };
+        // int K = arr.length;
+        // minTime(arr, N, K);
+
+        // String str = "3[b2[ca]]";
+        // System.out.println(decode(str));// TC O(n2logn)
+
+        // ArrayList<Integer> nums = new ArrayList<Integer>(List.of(4, 6, 3, 9, 10, 2));
+        // int count = minops(nums);// TC O(nlogn)
+        // System.out.println(count);
+
+        // int arr[] = { 10, 8, 6, 8, 9, 1 };
+        // System.out.println(HeapSort(arr));
+
+        // Sliding Window
+        // int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // int k = 3;
+        // int result[] = new int[arr.length - k + 1];
+
+        // PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        // for (int i = 0; i < k; i++) {
+        // pq.add(new Pair(arr[i], i));
+        // }
+
+        // result[0] = pq.peek().val;
+
+        // for (int i = k; i < arr.length; i++) {
+        // while (pq.size() > 0 && pq.peek().idx <= (i - k)) {
+        // pq.remove();
+        // }
+        // pq.add(new Pair(arr[i], i));
+        // result[i - k + 1] = pq.peek().val;
+        // }
+
+        // // print result
+        // for (int i = 0; i < result.length; i++) {
+        // System.out.println(result[i] + " ");
+        // }
+
+        // Weakest Soldiers
+        int army[][] = { { 1, 0, 0, 0 }, { 1, 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
+        int k = 2;
+
+        PriorityQueue<Row> pq = new PriorityQueue<Row>();
+
+        for (int i = 0; i < army.length; i++) {
+            int count = 0;
+            for (int j = 0; j < army[0].length; j++) {
+                count += army[i][j] == 1 ? 1 : 0;
+            }
+
+            pq.add(new Row(count, i));
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println('R' + pq.remove().index);
+        }
+
+    }
+}
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums.get(i);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < nums.size(); i++) {
+            pq.add(-nums.get(i));
+        }
+        double temp = sum;
+        int cnt = 0;
+        while (temp > sum / 2) {
+            int x = -pq.peek();
+            pq.remove();
+            temp -= Math.ceil(x * 1.0 / 2);
+            pq.add(x / 2);
+            cnt++;
+        }
+
+        return cnt;
+    }
+
     public static void main(String[] args) {
         // p = new PriorityQueue<>();
         // k = 3;
